014ac665307db1348bdcedd700603d418e084506
Umid Ergashev <umid2068066@gmail.com>
2025-08-13 13:41:43 +0500

    asdasd

--- operator/models/UpdateForm.php
+++ operator/models/UpdateForm.php
@@ -11,8 +11,13 @@ use yii\base\Model;
  * @property string $comment
  * @property string $send_time
  * @property string $take_time
+ * @property integer $region_id
+ * @property integer $district_id
  * @property integer $status
- * @property integer $competition
+ * @property integer $control
+ * @property integer $delivery_price
+ * @property integer|null $additional_phone
+ * @property string|null $additional_address
  */
 /**
  * Login form
@@ -23,10 +28,13 @@ class UpdateForm extends Model
     public $comment;
     public $send_time;
     public $status;
+    public $take_time;
+    public $control;
+    public $delivery_price;
+    public $additional_phone;
+    public $additional_address;
     public $region_id;
     public $district_id;
-    public $take_time;
-    public $competition;
 
 
     /**
@@ -36,82 +44,78 @@ class UpdateForm extends Model
     {
         return [
             [['region_id'], 'required', 'message' => Yii::t("app", "Please select a region!")],
+            [['district_id'],'required', 'message' => Yii::t("app", "Please select a district!"), 'when' => function($model){
+                return ($model->region_id == 1700) ? false : true;
+            }, 'whenClient' => "function (attribute, value) {
+                return $('#updateform-region_id').val() != 1700;
+            }"],
             [['status'], 'required', 'message' => Yii::t("app", "Please select a status!")],
-            [['competition'], 'required', 'message' => Yii::t("app", "Please choose delivery!")],
             [['comment'], 'required', 'message' => Yii::t("app", "Please enter more information!")],
             [['count'], 'required', 'message' => Yii::t("app", "Please select a count!")],
-            [['count', 'status', 'competition'] , 'integer'],
-            [['comment'], 'string'],
-            [['take_time'], 'string', 'max' => 255],
+            [['count', 'status', 'district_id', 'region_id', 'control'] , 'integer'],
+            [['comment', 'additional_phone', 'additional_address', 'take_time'], 'string'],
             [['take_time'],'required', 'message' => Yii::t("app", "Please set a time to pick up!"), 'when' => function($model){
                 return ($model->status == 5) ? true : false;
             }, 'whenClient' => "function (attribute, value) {
                 return $('#updateform-status').val() == 5;
             }"],
-            [['district_id'],'required', 'message' => Yii::t("app", "Please select a district!"), 'when' => function($model){
-                return ($model->region_id == 1700) ? false : true;
+            [['delivery_price'],'required', 'message' => Yii::t("app", "Please enter the shipping fee!"), 'when' => function($model){
+                return ($model->control == 1) ? true : false;
             }, 'whenClient' => "function (attribute, value) {
-                return $('#updateform-region_id').val() != 1700;
+                return $('#updateform-control').val() == 1;
             }"]
         ];
     }
 
     public function OrderUpdate($order){
 
-        if ($order->operator_id === null) {
+        if ($order->operator_id === null || $order->operator_id != Yii::$app->user->id) {
             return false;
         }
 
-        if ($this->status == 0) {
+        if ($this->status == Orders::STATUS_NEW) {
             $order->operator_id = null;
             $order->take_time = null;
         }
-        
-        if ($this->take_time) {
-            $order->take_time = strtotime($this->take_time);
-        }
-        
+
+        $order->take_time = strtotime($this->take_time) ?: $order->take_time;
+        $order->competition = ($this->control == 2) ? 0 : $this->delivery_price;
+        $order->is_then = $order->is_then == 1 ? 0 : $order->is_then;
+        $order->is_hold = $order->is_hold == 1 ? 0 : $order->is_hold;
         $order->status = $this->status;
+        $order->control_id = $this->control;
         $order->region_id = $this->region_id;
         $order->district_id = $this->district_id;
+        $order->additional_phone = $this->additional_phone;
+        $order->addres = $this->additional_address;
         $order->comment = $this->comment;
-        $order->competition = $this->competition;
         $order->count = $this->count;
         return $order->save(false);
     }
 
     public function LoadOrder($order){
-
-        if ($order->take_time != ''){
-            $this->take_time = $order->take_time;
-        }
-        $this->count = $order->count;
+        $this->take_time = $order->take_time ? Yii::$app->formatter->getDateWithoutTime($order->take_time) : $this->take_time;
+        $this->delivery_price = $order->competition ?? $this->delivery_price;
+        $this->control = $order->control_id;
         $this->region_id = $order->region_id;
         $this->district_id = $order->district_id;
-        $this->competition = $order->competition;
+        $this->count = $order->count;
+        $this->additional_phone = $order->additional_phone;
+        $this->additional_address = $order->addres;
         $this->comment = $order->comment;
     }
 
-    public function getStatus(): array
-    {
-        return [
-            Orders::STATUS_NEW => Yii::t("app", "New"),
-            Orders::STATUS_READY_TO_DELIVERY => Yii::t("app", "Ready for delivery"),
-            Orders::STATUS_THEN_TAKES => Yii::t("app", "Then takes"),
-            Orders::STATUS_HOLD => Yii::t("app", "Hold"),
-            Orders::STATUS_RETURNED_OPERATOR => Yii::t("app", "Archive"),
-            Orders::STATUS_BLACK_LIST => Yii::t("app", "Black list"),
-        ];
-    }
-
-    public function attributeLabels(): array
+    public function attributeLabels()
     {
         return [
             'id' => 'ID',
             'count' => Yii::t("app", "Count"),
             'comment' => Yii::t("app", "Additional Information"),
+            'additional_phone' => Yii::t("app", "Additional phone number"),
+            'additional_address' => Yii::t("app", "Additional address"),
             'status' => Yii::t("app", "Status"),
-            'competition' => Yii::t("app", "Delivery"),
+            'control' => Yii::t("app", "Delivery type"),
+            'delivery_price' => Yii::t("app", "Delivery price"),
             'district_id' => Yii::t("app", "District"),
             'region_id' => Yii::t("app", "Region"),
         ];
--- operator/views/operator/ajax.php
+++ operator/views/operator/ajax.php
@@ -3,12 +3,13 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\select2\Select2;
-use common\models\Regions;
-use yii\helpers\ArrayHelper;
-use kartik\depdrop\DepDrop;
 use kartik\touchspin\TouchSpin;
 use kartik\date\DatePicker;
+use yii\widgets\MaskedInput;
+use kartik\depdrop\DepDrop;
+use common\models\Regions;
 use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model operator\models\UpdateForm */
@@ -18,8 +19,16 @@ use yii\helpers\Url;
 $this->registerJs(<<<JS
 $(document).ready(function ($){
     let status = $order->status;
-    if (status != 5){
-        $('.field-updateform-take_time').css('display', 'none');
+    let control = $order->control_id;
+    let is_then = $order->is_then;
+    if (control == 1){
+        $('.field-updateform-delivery_price').css('display', 'block');
+    }else {
+        $('.field-updateform-delivery_price').css('display', 'none');
+    }
+    $('.field-updateform-take_time').css('display', 'none');
+    if (status == 5 || is_then == 1){
+        $('.field-updateform-take_time').css('display', 'block');
     }
 });
 $('#updateform-status').change(function (){
@@ -29,6 +38,15 @@ $('#updateform-status').change(function (){
         $('.field-updateform-take_time').css('display', 'block');
     }else{
         $('.field-updateform-take_time').css('display', 'none');
+    }
+});
+$('#updateform-control').change(function (){
+    let _this = $(this);
+    let control = _this.val();
+    if (control == 1){
+        $('.field-updateform-delivery_price').css('display', 'block');
+    }else{
+        $('.field-updateform-delivery_price').css('display', 'none');
     }
 });
 
@@ -41,108 +59,123 @@ JS
     <?php $form = ActiveForm::begin(); ?>
 
     <div class="row">
-        <?php if ($order->operator_id === null ):?>
-        <div class="col-md-6">
-            <p><?=Yii::t("app", "Order not accepted")?></p>
-        </div>
-        <?php else:?>
-        <div class="col-md-6">
-            <?= $form->field($model, 'region_id')->widget(Select2::classname(), [
-                'data' => ArrayHelper::map(Regions::find()->where(['parent_id' => 1])->all(), 'code', 'name'),
-                'hideSearch' => true,
-                'language' => 'uz',
-                'options' => [
-                    'placeholder' => Yii::t("app", "Select a region"),
-                    'class' => 'form-group has-feedback'
-                ],
-                'pluginOptions' => [
-                    'allowClear' => true
-                ],
-            ]);
-            ?>
-        </div>
-        <div class="col-md-6">
-            <?= $form->field($model, 'district_id')->widget(DepDrop::classname(), [
-                'language' => 'ru',
-                'options' => [
-                    'placeholder' => Yii::t("app", "Select a district"),
-                    'class' => 'form-group has-feedback'
-                ],
-                'type' => DepDrop::TYPE_SELECT2,
-                'pluginOptions' => [
-                    'initialize' => true,
-                    'allowClear' => true,
-                    'depends' => ['updateform-region_id'],
-                    'url' => Url::to(['/site/territorial-administrations']),
-                    'loadingText' => Yii::t("app", "Please wait!"),
-                ],
-            ])?>
-        </div>
-        <div class="col-md-6">
-            <?= $form->field($model, 'status')->widget(Select2::classname(), [
-                'data' => $model->getStatus(),
-                'language' => 'ru',
-                'options' => ['placeholder' => Yii::t("app", "Select a status")],
-                'pluginOptions' => [
-                    'allowClear' => true
-                ],
-            ]);
-            ?>
-        </div>
-        <div class="col-md-6">
-            <?= $form->field($model, 'take_time')->widget(DatePicker::classname(), [
-                'name' => 'check_issue_date',
-                'value' => date('Y-m-d', strtotime('+2 days')),
-                'options' => [
-                        'placeholder' => Yii::t("app", "Set the pick-up time"),
-                ],
-                'pluginOptions' => [
-                    'format' => 'yyyy-mm-dd',
-                    'todayHighlight' => true
-                ]
-            ])->label('Olish vaqti');
-            ?>
-        </div>
-        <div class="col-md-6">
-            <?= $form->field($model, 'competition')->widget(Select2::classname(), [
-                'data' => [
-                    1 => Yii::t("app", "Delivery is paid"),
-                    0 => Yii::t("app", "Free shipping")
-                ],
-                'language' => 'ru',
-                'options' => ['placeholder' => Yii::t("app", "Select a delivery")]
-            ]);
-            ?>
-        </div>
-        <div class="col-md-6">
-            <?= $form->field($model, 'count')->widget(TouchSpin::classname(), [
-                'name' => 'volume',
-                'options' => ['placeholder' => Yii::t("app", "Enter the number")],
-                'pluginOptions' => [
-                    'buttonup_class' => 'btn btn-primary',
-                    'buttondown_class' => 'btn btn-info',
-                    'buttonup_txt' => '<i class="glyphicon glyphicon-plus-sign"></i>',
-                    'buttondown_txt' => '<i class="glyphicon glyphicon-minus-sign"></i>'
-                ]
-            ]);
-            ?>
-        </div>
-        <div class="col-md-12">
-            <?= $form->field($model, 'comment')->textarea(['maxlength' => true]) ?>
-        </div>
-        <div class="col-md-12">
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t("app", "Submit"), ['class' => 'btn btn-success']) ?>
+        <?php if ($order->operator_id == Yii::$app->user->id):?>
+            <div class="col-md-6">
+                <?= $form->field($model, 'region_id')->widget(Select2::classname(), [
+                    'data' => ArrayHelper::map(Regions::find()->where(['parent_id' => 1])->all(),'code','name'),
+                    'hideSearch' => true,
+                    'language' => 'uz',
+                    'options' => [
+                        'placeholder' => Yii::t("app","Select a region"),
+                        'class' => 'form-group has-feedback'
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);
+                ?>
             </div>
-        </div>
-        <div class="col-md-6">
-            <p><?=$order->product->descriptionTranslate?></p>
-        </div>
-        <div class="col-md-6">
-            <h1><?=$order->product->titleTranslate?></h1>
-            <h5><?=$order->product->sale?> SUMM</h5>
-            <h5>Omborda <?=$order->product->in_stock ?> dona qoldi</h5>
-        </div>
+            <div class="col-md-6">
+                <?= $form->field($model, 'district_id')->widget(DepDrop::classname(), [
+                    'language' => 'ru',
+                    'options' => [
+                        'placeholder' => Yii::t("app","Select a district"),
+                        'class' => 'form-group has-feedback'
+                    ],
+                    'type' => DepDrop::TYPE_SELECT2,
+                    'pluginOptions' => [
+                        'initialize' => true,
+                        'allowClear' => true,
+                        'depends' => ['updateform-region_id'],
+                        'url' => Url::to(['/site/territorial-administrations']),
+                        'loadingText' => Yii::t("app","Please wait!"),
+                    ],
+                ])?>
+            </div>
+            <div class="col-md-6">
+                <?= $form->field($model, 'control')->widget(Select2::classname(), [
+                    'data' => [
+                        1 => Yii::t("app","Delivery is paid"),
+                        2 => Yii::t("app","Free shipping")
+                    ],
+                    'language' => 'ru',
+                    'options' => ['placeholder' => Yii::t("app", "Select a delivery")]
+                ]);
+                ?>
+            </div>
+            <div class="col-md-6">
+                <?= $form->field($model,'delivery_price')->textInput(['value' => $model->delivery_price ?: 30000])?>
+            </div>
+            <div class="col-md-12">
+                <?= $form->field($model,'additional_address')->textarea(['maxlength' => true]) ?>
+            </div>
+            <div class="col-md-12">
+                <?= $form->field($model,'comment')->textarea(['maxlength' => true]) ?>
+            </div>
+            <div class="col-md-6">
+                <?= $form->field($model,'additional_phone')->widget(MaskedInput::className(), [
+                    'mask' => '+\\9\\98(99)-999-99-99',
+                    'options' => [
+                        'placeholder' => \Yii::t("app","Additional phone number")
+                    ]
+                ])
+                ?>
+            </div>
+            <div class="col-md-6">
+                <?= $form->field($model,'status')->widget(Select2::classname(), [
+                    'data' => Yii::$app->status->arrayStatusForOperator(),
+                    'language' => 'ru',
+                    'options' => ['placeholder' => Yii::t("app","Select a status")],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ]
+                ]);
+                ?>
+            </div>
+            <div class="col-md-12">
+                <?= $form->field($model, 'take_time')->widget(DatePicker::classname(), [
+                    'name' => 'check_issue_date',
+                    'options' => [
+                        'placeholder' => Yii::t("app", "Set the pick-up time")
+                    ],
+                    'pluginOptions' => [
+                        'format' => 'dd.mm.yyyy',
+                        'todayHighlight' => true
+                    ]
+                ])->label('Olish vaqti');
+                ?>
+            </div>
+            <div class="col-md-12">
+                <?= $form->field($model,'count')->widget(TouchSpin::classname(), [
+                    'name' => 'volume',
+                    'options' => ['placeholder' => Yii::t("app","Enter the number")],
+                    'pluginOptions' => [
+                        'buttonup_class' => 'btn btn-primary',
+                        'buttondown_class' => 'btn btn-info',
+                        'buttonup_txt' => '<i class="glyphicon glyphicon-plus-sign"></i>',
+                        'buttondown_txt' => '<i class="glyphicon glyphicon-minus-sign"></i>'
+                    ]
+                ]);
+                ?>
+            </div>
+
+            <div class="col-md-12">
+                <div class="form-group">
+                    <?= Html::submitButton(Yii::t("app", "Submit"), ['class' => 'btn btn-success']) ?>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <p><?=$order->product->descriptionTranslate?></p>
+            </div>
+            <div class="col-md-6">
+                <h1><?=$order->product->titleTranslate?></h1>
+                <h5><?=$order->product->sale?> SUMM</h5>
+                <h5>Omborda <?=$order->product->in_stock ?> dona qoldi</h5>
+            </div>
+        <?php else:?>
+            <div class="col-md-6">
+                <p><?=Yii::t("app", "Order not accepted")?></p>
+            </div>
         <?php endif;?>
     </div>
 
